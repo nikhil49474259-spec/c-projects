4c1cbfca99a27b2249742197c1e346d71c524770
Nikhil <nikhil49474259@gmail.com>
2026-07-28 12:43:34 +0530

    first C project

---
+++ introduction.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main(){
+
+
+// This is my first program!:)
+/* This is my first task */
+
+
+
+
+
+
+
+	printf("I like my mom!\n");
+	printf("because she makes me everytime happy");
+
+	return 0;
+}
+
+
