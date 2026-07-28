49761391e5456694d836e03b8475fd058efceb2a
Nikhil <nikhil49474259@gmail.com>
2026-07-28 15:48:42 +0530

    modified introduction.c

--- introduction.c
+++ introduction.c
@@ -7,7 +7,13 @@ int main(){
 /* This is my first task */
 
 
+int age = 16
+int year = 2026
+int quantity = 6
 
+printf("I'm %d years old\n", age);
+printf("The year %d is very weird\n", year);
+printf("I bought %d cakes for my birthday\n", quantity);
 
 
 
