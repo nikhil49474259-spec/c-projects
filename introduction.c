b5f67af42b4a2a394ea82d34f7157003d09f5a84
Nikhil <nikhil49474259@gmail.com>
2026-07-28 16:12:05 +0530

    modified introduction.c

--- introduction.c
+++ introduction.c
@@ -7,15 +7,21 @@ int main(){
 /* This is my first task */
 
 
-int age = 16
-int year = 2026
-int quantity = 6
+int age = 16;
+int year = 2026;
+int quantity = 6;
 
 printf("I'm %d years old\n", age);
 printf("The year %d is very weird\n", year);
 printf("I bought %d cakes for my birthday\n", quantity);
 
+float gpa = 9.7;
+float price = 21.198;
+float temperature = 100.0;
 
+printf("My 10th class gpa is %f\n", gpa);
+printf("The price of the cup is ₹ .1%f\n", price);
+printf("The temperature at whuch water boils is %f °C \n", temperature);
 
 
 	printf("I like my mom!\n");
