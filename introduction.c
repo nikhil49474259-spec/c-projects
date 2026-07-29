25eaed2a9f7fecb7419434a8a1ee73468cab48c7
Nikhil <nikhil49474259@gmail.com>
2026-07-29 07:19:12 +0530

    Remodified introduction.c

--- introduction.c
+++ introduction.c
@@ -1,4 +1,6 @@
 #include <stdio.h>
+#include <stdbool.h>
+
 
 int main(){
 
@@ -22,6 +24,37 @@ float temperature = 100.0;
 printf("My 10th class gpa is %f\n", gpa);
 printf("The price of the cup is ₹ %f\n", price);
 printf("The temperature at whuch water boils is %f °C \n", temperature);
+
+double pi = 3.1415926;
+double e = 2.718281828;
+
+printf("The value of e is %.9lf\n", e);
+printf("The value of pi is %.9lf\n", pi);
+
+
+
+bool is_online = 1;
+bool is_student =0;
+
+if (is_online){
+	printf("You are online")
+}
+
+else{
+	printf("You are offline")
+}
+
+
+if (is_student){
+	printf("You are a student")
+}
+
+else{
+	printf("You are not a student")
+}
+
+
+
 
 
 	printf("I like my mom!\n");
